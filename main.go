b58c82dd9537ab322d4bbcf9fa2258027cbd290e
quangdangfit <quangdangfit@gmail.com>
2020-04-29 11:48:09 +0700

    custom main file

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	"os"
+	"os/signal"
 	"qcafe/routers"
+	"time"
 )
 
 func main() {
@@ -10,5 +15,20 @@ func main() {
 
 	routers.SetRouters(e)
 
-	e.Logger.Fatal(e.Start(":8080"))
+	go func() {
+		port := 8080
+		log.Info("Starting at port: %d", port)
+		e.Start(":" + string(port))
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 10 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
